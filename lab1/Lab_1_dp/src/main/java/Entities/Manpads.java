4e16047d5cc26bb848393e6f29a26d3185656d52
Vetal0205 <vetal0205merkulov@gmail.com>
2023-01-27 19:43:54 +0100

    Lab1_dp-pilotVersion (html + scss + doget from backend)

---
+++ lab1/Lab_1_dp/src/main/java/Entities/Manpads.java
@@ -0,0 +1,37 @@
+package Entities;
+
+public class Manpads {
+    private String name;
+    private double weight;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Manpads(String name, double weight, String photo) {
+        this.name = name;
+        this.weight = weight;
+        this.photo = photo;
+    }
+}
